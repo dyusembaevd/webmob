5cb29d5af8c4b71036544e61af684292d140ef24
akhmetzhan <Raikvaza@gmail.com>
2024-10-24 17:34:22 +0500

    ref: projects registry refactor

--- src/app/[locale]/projects/page.tsx
+++ src/app/[locale]/projects/page.tsx
@@ -1,10 +1,8 @@
 "use client";
 
 import { config } from "@/config";
-import { CategoryBadge } from "@/entities/blogger/ui/CategoryBadge";
 import { Project } from "@/entities/project/types";
 import { useRouter } from "@/navigation";
-import IconPen from "@/shared/assets/icons/icon_pen.svg";
 import { MainLayout } from "@/shared/layouts";
 import { Button } from "@/shared/ui/Button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/shared/ui/Tabs";
@@ -33,7 +31,6 @@ const getAds = async ({
 
 export default function ProjectPage() {
   const locale = useLocale();
-  const router = useRouter();
   const { data, isLoading } = useQuery<Project[]>({
     queryKey: ["ads"],
     queryFn: () => getAds({ locale: locale as "ru" | "kz" | "en" }),
@@ -50,8 +47,7 @@ export default function ProjectPage() {
         </div>
         <Tabs
           defaultValue="projects"
-          defaultChecked
-          className="flex flex-1 shrink-0 grow flex-col bg-[#F3F4F1] pb-24 pt-5"
+          className="flex flex-1 flex-col bg-[#F3F4F1] pb-24 pt-5"
         >
           <TabsList>
             <TabsTrigger value="projects">Мои проекты</TabsTrigger>
@@ -72,100 +68,21 @@ export default function ProjectPage() {
                     ))}
               </div>
             </TabsContent>
+
             <TabsContent value="active">
-              <div className="flex flex-col items-stretch justify-start gap-4">
-                <div className="flex flex-col items-center justify-start gap-8">
-                  <Image
-                    src={`${config.BASE_URL}/tmp/img_projects_active.png`}
-                    width={216}
-                    quality={100}
-                    height={179}
-                    alt=""
-                  />
-                  <div className="flex flex-col items-stretch justify-start gap-2">
-                    <Typography className="text-center text-[20px] font-semibold leading-[28px]">
-                      У вас пока нет проектов в работе
-                    </Typography>
-                    <Typography className="text-center text-[15px] leading-[21px]">
-                      Вы можете предложить свой проект блогерам в разделе
-                      «Исполнители» или на главном экране
-                    </Typography>
-                  </div>
-                  <Button
-                    onClick={() => router.push("/")}
-                    className={cn("w-full text-white", "flex-1 bg-[#8065FF]")}
-                  >
-                    Найти исполнителя
-                  </Button>
-                </div>
-              </div>
+              <ActiveProjectsEmptyTab />
             </TabsContent>
+
             <TabsContent value="offers">
-              <div className="flex flex-col items-stretch justify-start gap-4">
-                <div className="flex flex-col items-center justify-start gap-8">
-                  <Image
-                    src={`${config.BASE_URL}/tmp/img_projects_offers.png`}
-                    width={155}
-                    quality={100}
-                    height={200}
-                    alt=""
-                  />
-                  <div className="flex flex-col items-stretch justify-start gap-2">
-                    <Typography className="text-center text-[20px] font-semibold leading-[28px]">
-                      У вас пока нет персональных предложений{" "}
-                    </Typography>
-                    <Typography className="text-center text-[15px] leading-[21px]">
-                      Как только исполнитель предложит вам сотрудничество, его
-                      визитка появится здесь
-                    </Typography>
-                  </div>
-                </div>
-              </div>
+              <OffersEmptyTab />
             </TabsContent>
+
             <TabsContent value="favourite">
-              <div className="flex flex-col items-stretch justify-start gap-4">
-                <div className="flex flex-col items-center justify-start gap-8">
-                  <Image
-                    src={`${config.BASE_URL}/tmp/img_projects_favourite.png`}
-                    width={149}
-                    quality={100}
-                    height={200}
-                    alt=""
-                  />
-                  <div className="flex flex-col items-stretch justify-start gap-2">
-                    <Typography className="text-center text-[20px] font-semibold leading-[28px]">
-                      В избранном пока ничего нет{" "}
-                    </Typography>
-                    <Typography className="text-center text-[15px] leading-[21px]">
-                      Не теряйте понравившихся блогеров. Добавляйте их визитки
-                      в ”Избранное» и приступайте к работе в удобное
-                      для вас время
-                    </Typography>
-                  </div>
-                </div>
-              </div>
+              <FavouriteEmptyTab />
             </TabsContent>
+
             <TabsContent value="archive">
-              <div className="flex flex-col items-stretch justify-start gap-4">
-                <div className="flex flex-col items-center justify-start gap-8">
-                  <Image
-                    src={`${config.BASE_URL}/tmp/img_projects_archive.png`}
-                    width={162}
-                    quality={100}
-                    height={200}
-                    alt=""
-                  />
-                  <div className="flex flex-col items-stretch justify-start gap-2">
-                    <Typography className="text-center text-[20px] font-semibold leading-[28px]">
-                      Здесь будет история ваших проектов{" "}
-                    </Typography>
-                    <Typography className="text-center text-[15px] leading-[21px]">
-                      Любые проекты, которые были завершены или отменены по
-                      какой-либо причине, будут храниться в архиве
-                    </Typography>
-                  </div>
-                </div>
-              </div>
+              <ArchiveEmptyTab />
             </TabsContent>
           </div>
         </Tabs>
@@ -173,3 +90,110 @@ export default function ProjectPage() {
     </MainLayout>
   );
 }
+
+const ActiveProjectsEmptyTab: React.FC = () => {
+  const router = useRouter();
+  return (
+    <div className="flex flex-col items-stretch justify-start gap-4">
+      <div className="flex flex-col items-center justify-start gap-8">
+        <Image
+          src={`${config.BASE_URL}/tmp/img_projects_active.png`}
+          width={216}
+          quality={100}
+          height={179}
+          alt=""
+        />
+        <div className="flex flex-col items-stretch justify-start gap-2">
+          <Typography className="text-center text-[20px] font-semibold leading-[28px]">
+            У вас пока нет проектов в работе
+          </Typography>
+          <Typography className="text-center text-[15px] leading-[21px]">
+            Вы можете предложить свой проект блогерам в разделе «Исполнители»
+            или на главном экране
+          </Typography>
+        </div>
+        <Button
+          onClick={() => router.push("/")}
+          className={cn("w-full text-white", "flex-1 bg-[#8065FF]")}
+        >
+          Найти исполнителя
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+const OffersEmptyTab: React.FC = () => {
+  return (
+    <div className="flex flex-col items-stretch justify-start gap-4">
+      <div className="flex flex-col items-center justify-start gap-8">
+        <Image
+          src={`${config.BASE_URL}/tmp/img_projects_offers.png`}
+          width={155}
+          quality={100}
+          height={200}
+          alt=""
+        />
+        <div className="flex flex-col items-stretch justify-start gap-2">
+          <Typography className="text-center text-[20px] font-semibold leading-[28px]">
+            У вас пока нет персональных предложений
+          </Typography>
+          <Typography className="text-center text-[15px] leading-[21px]">
+            Как только исполнитель предложит вам сотрудничество, его визитка
+            появится здесь
+          </Typography>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const FavouriteEmptyTab: React.FC = () => {
+  return (
+    <div className="flex flex-col items-stretch justify-start gap-4">
+      <div className="flex flex-col items-center justify-start gap-8">
+        <Image
+          src={`${config.BASE_URL}/tmp/img_projects_favourite.png`}
+          width={149}
+          quality={100}
+          height={200}
+          alt=""
+        />
+        <div className="flex flex-col items-stretch justify-start gap-2">
+          <Typography className="text-center text-[20px] font-semibold leading-[28px]">
+            В избранном пока ничего нет
+          </Typography>
+          <Typography className="text-center text-[15px] leading-[21px]">
+            Не теряйте понравившихся блогеров. Добавляйте их визитки в
+            «Избранное» и приступайте к работе в удобное для вас время
+          </Typography>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const ArchiveEmptyTab: React.FC = () => {
+  return (
+    <div className="flex flex-col items-stretch justify-start gap-4">
+      <div className="flex flex-col items-center justify-start gap-8">
+        <Image
+          src={`${config.BASE_URL}/tmp/img_projects_archive.png`}
+          width={162}
+          quality={100}
+          height={200}
+          alt=""
+        />
+        <div className="flex flex-col items-stretch justify-start gap-2">
+          <Typography className="text-center text-[20px] font-semibold leading-[28px]">
+            Здесь будет история ваших проектов
+          </Typography>
+          <Typography className="text-center text-[15px] leading-[21px]">
+            Любые проекты, которые были завершены или отменены по какой-либо
+            причине, будут храниться в архиве
+          </Typography>
+        </div>
+      </div>
+    </div>
+  );
+};
